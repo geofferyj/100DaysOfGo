6fba0397e38d7ad3ebeec1396af312e40e6100bd
geofferyj <geofferyjoseph1@gmail.com>
2023-04-28 23:32:30 +0100

    day 2 done

---
+++ day_2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var var1 int
+var var2 int = 20
+
+var var3 = 30
+
+var var5, var6 int = 50, 60
+
+
+func main() {
+	var1 = 10
+	var4 := 40
+
+	fmt.Println(var1)
+	fmt.Println(var2)
+	fmt.Println(var3)
+	fmt.Println(var4)
+
+
+}
