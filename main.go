35ca186f1ee7caa077cf245bc51e79a87b9a7e38
geofferyj <geofferyjoseph1@gmail.com>
2023-05-03 21:16:43 +0100

    Day 3 complete

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+	int1 := 1
+	int2 := 2
+
+	fmt.Println("1 + 2 = ", int1 + int2)
+	fmt.Println("1 - 2 = ", int1 - int2)
+	fmt.Println("1 * 2 = ", int1 * int2)
+	fmt.Println("1 / 2 = ", int1 / int2)
+
+
+	
+}
