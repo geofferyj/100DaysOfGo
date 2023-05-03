1ab8e8ef899c37d9af1fadfce2cc9efe99cbc166
geofferyj <geofferyjoseph1@gmail.com>
2023-05-03 23:44:34 +0100

    finished day 5

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	int1 := 5
+	int2 := 10
+
+	fmt.Printf("Sum of %v and %v is %d\n", int1, int2, int1 + int2)
+	fmt.Printf("Difference Between %v and %v is %d\n", int1, int2, int1 - int2)
+	fmt.Printf("Product of %v and %v is %d\n", int1, int2, int1 * int2)
+	fmt.Printf("Quotient of %v and %v is %d\n", int1, int2, int1 / int2)
+
+}
